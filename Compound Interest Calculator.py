c41a352a8a037c634421747b112d537614e03e26
CypherLegacy <70846136+CypherLegacy@users.noreply.github.com>
2021-04-09 13:11:04 -0400

    Update Compound Interest Calculator.py

--- Compound Interest Calculator.py
+++ Compound Interest Calculator.py
@@ -7,20 +7,20 @@ def cpdint():
     b = 0
     n = '\n'
     s = '     '
-    a = int(input(f'{n}How many items do you have to calculate? '))
+    a = int(input(f'{n}How many accounts do you want to calculate? '))
     while b < a:
-        prin = float(input(f"{n}What is the principal amount? "))
-        rate = float(input(f"{n}What is the interest rate? "))
-        time = int(input(f"{n}What is the duration in years? "))
-        number = int(input(f"{n}How many times will interest compound upon each year? (Note: minimum is 1): "))
-        print(f'{n*2}{s}FINAL VALUES...{n}')
-        print(f'{n*2}{s}Principal = {prin}'); print(f'{s}Interest rate = {rate}');
-        print(f'{s}Years = {time}'); print(f'{s}Compounding = {number}{n*2}')
+        prin = float(input(f"What is the principal amount? "))
+        rate = float(input(f"What is the interest rate (in decimal format)? "))
+        time = int(input(f"What is the duration in years? "))
+        number = int(input(f"How many times will interest be compounded each year? (Note: minimum is 1): "))
+        print(f'{n*2}{s}FINAL VALUES...')
+        print(f'{n*2}{s}Principal = {prin}'); print(f'{s}Interest rate = {rate}'); print(f'{s}Years = {time}');
+        print(f'{s}Compounding = {number}{n*2}')
 
         amt1 = rate / number
         amt2 = time*number
         total = prin*(1+amt1)**amt2
-        print(f'{n}Your total is: {total}{n}')
+        print(f'Your total is: {total}{n}')
         b = b + 1
 
 #Conditional statement which triggers the program.
