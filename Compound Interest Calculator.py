7d0dacd36c08d38755a4498b29b4a77b4ee4ab36
CypherLegacy <70846136+CypherLegacy@users.noreply.github.com>
2021-04-09 14:04:48 -0400

    Updated compound interest calculator.py

--- Compound Interest Calculator.py
+++ Compound Interest Calculator.py
@@ -20,7 +20,7 @@ def cpdint():
         amt1 = rate / number
         amt2 = time*number
         total = prin*(1+amt1)**amt2
-        print(f'Your total is: {total}{n}')
+        print(f'Your total is: {total}')
         b = b + 1
 
 #Conditional statement which triggers the program.
